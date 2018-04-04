3eb5e601f79ae113616c5649fe8ed7db8891986d
Truong Vinh Long <long_tv@septeni-technology.jp>
2018-04-04 18:18:02 +0700

    setup header

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+ <header class="App-header">
+  <img id="logo" src="../assets/logo.png" class="App-logo" alt="Tanks.vn logo"/>
+  <div class="App-title">Tanks.vn</div>
+  <div class="App-intro">FB page: <a href='http://facebook.com/tanksvn'>http://facebook.com/tanksvn</a> - Hotline: 0125.737.9966</div>
+ </header>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader'
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import lightbox from 'vlightbox'
+Vue.use(lightbox)
 
 Vue.config.productionTip = false
 
