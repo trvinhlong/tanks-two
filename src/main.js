c4ce9f69c7127e6d99b5ef90d71ba208c3b1ee9a
Truong Vinh Long <long_tv@septeni-technology.jp>
2018-04-11 14:14:38 +0700

    add GA

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import lightbox from 'vlightbox'
+import VueAnalytics from 'vue-analytics'
+import Config from './Config'
+
+Vue.use(VueAnalytics, {
+    id: Config.gaTrackingId
+})
 Vue.use(lightbox)
 
 Vue.config.productionTip = false
